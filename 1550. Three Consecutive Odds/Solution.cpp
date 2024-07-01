29980086afb88c053cb047ac59b6141cfa024f67
Dharmanshuj <124305189+Dharmanshuj@users.noreply.github.com>
2024-07-02 00:49:57 +0530

    Update Solution.cpp

--- 1550. Three Consecutive Odds/Solution.cpp
+++ 1550. Three Consecutive Odds/Solution.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr)
+    {
+        if(arr.size() < 3)
+            return false;
+        int i=0;
+        while(i < arr.size()-2)
+        {
+            if(arr[i+2]%2 == 0)
+                i = i+3;
+            else if(arr[i+1]%2 == 0)
+                i = i+2;
+            else if(arr[i]%2 == 0)
+                i = i+1;
+            else
+                return true; 
+        }
+        return false;
+    }
+};
